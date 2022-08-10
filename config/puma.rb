6dc167d80111f79ba6e77a34c0e78d0f96b61eb7
Alexandr Babich <alexandr.babich@appodeal.com>
2022-08-10 22:31:38 +0200

    setup prod db

--- config/puma.rb
+++ config/puma.rb
@@ -15,14 +15,29 @@ worker_timeout 3600 if ENV.fetch("RAILS_ENV", "development") == "development"
 
 # Specifies the `port` that Puma will listen on to receive requests; default is 3000.
 #
-port ENV.fetch("PORT") { 3000 }
+
 
 # Specifies the `environment` that Puma will run in.
 #
 environment ENV.fetch("RAILS_ENV") { "development" }
+rails_env = ENV.fetch("RAILS_ENV") { "development" }
 
 # Specifies the `pidfile` that Puma will use.
 pidfile ENV.fetch("PIDFILE") { "tmp/pids/server.pid" }
+
+app_dir    = File.expand_path("../..", __FILE__)
+shared_dir = "#{app_dir}/tmp"
+
+puts app_dir
+
+if rails_env == 'production'
+  bind "unix://#{shared_dir}/sockets/puma.sock"
+  rackup "#{app_dir}/config.ru"
+else
+  port ENV.fetch("PORT") { 3000 }
+  plugin :tmp_restart
+end
+
 
 # Specifies the number of `workers` to boot in clustered mode.
 # Workers are forked web server processes. If using threads and workers together
@@ -40,4 +55,4 @@ pidfile ENV.fetch("PIDFILE") { "tmp/pids/server.pid" }
 # preload_app!
 
 # Allow puma to be restarted by `bin/rails restart` command.
-plugin :tmp_restart
+
