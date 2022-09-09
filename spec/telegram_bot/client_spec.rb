0aee0eb050db7c60af912fefacc7edf7e60fd9c3
Alexandr Babich <alexandr.babich@appodeal.com>
2022-09-09 18:08:34 +0200

    clean test

--- spec/telegram_bot/client_spec.rb
+++ spec/telegram_bot/client_spec.rb
@@ -1,20 +1,17 @@
 # frozen_string_literal: true
 
 require 'rails_helper'
-#require 'telegram_bot/client'
 
 RSpec.describe TelegramBot::Client do
-  let(:client) { described_class.new }
-
   describe "Client yields message" do
     context "Mesage received" do
       let(:message) { create(:message) }
 
 
       it "yields message" do
-        allow(client).to receive(:get_updates).and_yield(message)
+        allow(subject).to receive(:get_updates).and_yield(message)
 
-        client.get_updates do |m|
+        subject.get_updates do |m|
           expect(m).to eq(message)
         end
       end
