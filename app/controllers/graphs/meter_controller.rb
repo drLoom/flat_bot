144a19409d46e635a5fc240a990250d0b93e4cd8
Alexandr Babich <alexandr.babich@appodeal.com>
2022-10-02 18:13:56 +0200

    cache

--- app/controllers/graphs/meter_controller.rb
+++ app/controllers/graphs/meter_controller.rb
@@ -3,7 +3,9 @@
 module Graphs
   class MeterController < ApplicationController
     def index
-      @hists = FlatsHist.group(:date).average('price_usd / area').to_json
+      @hists = Rails.cache.fetch("MeterController#hists", expires_in: 1.hours) do
+        FlatsHist.group(:date).average('price_usd / area').to_json
+      end
     end
   end
 end
