763083ad5cb3a1b59af688a0ab563e4899a3dde0
Alexandr Babich <alexandr.babich@appodeal.com>
2023-01-16 11:45:30 +0100

    conver from relation

--- app/controllers/tops_controller.rb
+++ app/controllers/tops_controller.rb
@@ -3,13 +3,13 @@
 class TopsController < ApplicationController
   def top_flats
     @top_flats = Rails.cache.fetch("TopsController#top_flats_#{rooms}", expires_in: 1.hours) do
-      FlatsHist.recent.where(rooms: rooms).order(price_usd: :desc).limit(15)
+      FlatsHist.recent.where(rooms: rooms).order(price_usd: :desc).limit(15).to_a
     end
   end
 
   def top_newly
     @top_newly = Rails.cache.fetch("TopsController#top_newly#{rooms}", expires_in: 1.hours) do
-      FlatsHist.newly.where(rooms: rooms).order(price_usd: :desc).limit(15)
+      FlatsHist.newly.where(rooms: rooms).order(price_usd: :desc).limit(15).to_a
     end
   end
 
