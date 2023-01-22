fd9db83353e1abd53996031f2052750910e8ffe9
Alexandr Babich <alexandr.babich@appodeal.com>
2023-01-22 13:31:36 +0100

    speed up img_id

--- lib/flats/set_internal_id.rb
+++ lib/flats/set_internal_id.rb
@@ -15,12 +15,25 @@ module Flats
     end
 
     def img_id
-      count = FlatsHist.count
+      hists = FlatsHist.select('DISTINCT ON (object_id) flats_hist.*')
+      count = hists.size
+
       i = 0
-      FlatsHist.find_each do |flat|
-        flat.img_id = flat.img_key
-        flat.save!
-        i += 1
+      hists.each_slice(1000) do |batch|
+        object_id_img_key = batch.map { |fh| "(#{fh.object_id}, #{fh.img_key})" }
+
+        query = <<~SQL
+          UPDATE flats_hist
+          SET img_id=incoming.img_key
+          FROM (
+            values #{object_id_img_key.join(', ')}
+          ) incoming(object_id, img_key)
+          WHERE flats_hist.object_id=incoming.object_id;
+        SQL
+
+        ApplicationRecord.connection.execute(query)
+
+        i += batch.size
 
         print "#{i} / #{count} (#{(i.to_f / count * 100).round} %)".ljust(30) + "\r"
       end
